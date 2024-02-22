fa1198f237793bd18377c2d60ed93159517fd977
Xu Liu <sureiamlx@outlook.com>
2024-02-22 11:40:21 +0800

    调整目录结构

--- MainApplication.java
+++ MainApplication.java
@@ -4,8 +4,6 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-import classes.*;
-
 
 public class MainApplication {
     public static void main(String[] args) throws Exception {
